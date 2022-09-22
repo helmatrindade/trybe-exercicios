9c5993daa0302320ebd9d1ac22a81d125f9eacc7
helmatrindade <helmacaires28@gmail.com>
2022-09-22 20:05:03 -0300

    exercicio

---
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/script.js
@@ -0,0 +1,19 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for(let index = 0; index < numbers.length; index += 1){
+    console.log(numbers[index]);
+}
+
+// soma todos valores contidos no array
+let soma = 0;
+for(index = 0; index < numbers.length; index += 1){
+    soma += numbers[index];
+}
+console.log(soma);
+
+// média aritmética dos valores contido no array
+let mediaAritimetica = 0;
+for(let index = 0; index < numbers.length; index += 1){
+    mediaAritimetica + numbers.length[index];
+}
+console.log(mediaAritimetica / numbers.length);
