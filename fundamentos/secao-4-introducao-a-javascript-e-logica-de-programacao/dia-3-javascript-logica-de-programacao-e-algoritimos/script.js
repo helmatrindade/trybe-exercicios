31f62d1f721f73a1ed9b2dc6073c54aa93c4a027
helmatrindade <helmacaires28@gmail.com>
2022-09-25 17:28:18 -0300

    inverter palavra

--- fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritimos/script.js
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritimos/script.js
@@ -1,6 +1,18 @@
+//numero fatorial
+
 let number = 1;
 
 for( let index = 10; index > 0; index -= 1){
     number *= index;
 }
 console.log(number);
+
+//Inverter palavra
+
+let word = 'tryber';
+let newWord = '';
+for(let index = 0; index < word.length; index += 1){
+    newWord += word[word.length - 1 - index];
+}
+console.log(newWord);
+
