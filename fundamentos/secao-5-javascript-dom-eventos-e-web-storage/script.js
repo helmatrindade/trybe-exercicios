76cd33990127fdc90466e87e7ce7d8a3d2adadf0
helmatrindade <helmacaires28@gmail.com>
2022-09-30 09:22:30 -0300

    cria arquivo index.html e script.js

---
+++ fundamentos/secao-5-javascript-dom-eventos-e-web-storage/script.js
@@ -0,0 +1,13 @@
+let exercicio1 = document.getElementById('exercice');
+
+function adicionaTexto(){
+let resolvendoExercicio = document.createElement('h1');
+document.getElementById('exercice').innerText = 'Exercício - JavaScript DOM';
+}
+adicionaTexto();
+
+let exercicio2 = document.getElementsByClassName(main-content);
+function tagMain(){
+    let criaMain = document.createElement('main');
+    criaMain.appendChild(exercicio2);
+}
