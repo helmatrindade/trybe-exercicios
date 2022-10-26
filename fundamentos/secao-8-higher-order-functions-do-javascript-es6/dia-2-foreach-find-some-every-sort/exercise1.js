d0f1517386f8cdab5cae52679a820d1895b3f969
helmatrindade <helmacaires28@gmail.com>
2022-10-26 18:19:50 -0300

    Alterações

--- fundamentos/secao-8-higher-order-functions-do-javascript-es6/dia-2-foreach-find-some-every-sort/exercise1.js
+++ fundamentos/secao-8-higher-order-functions-do-javascript-es6/dia-2-foreach-find-some-every-sort/exercise1.js
@@ -83,8 +83,21 @@ function smallerName() {
 
 console.log(smallerName());
 
-//Encontre o primeiro livro cujo nome possui 26 caracteres.
 
+//Encontre o primeiro livro cujo nome possui 26 caracteres.
+const expectedResult = {
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: {
+        name: 'George R. R. Martin',
+    birthYear: 1948,
+  },
+    releaseYear: 1991,
+};
+function getNamedBook() {
+  // escreva seu código aqui
+}
 
 
 
