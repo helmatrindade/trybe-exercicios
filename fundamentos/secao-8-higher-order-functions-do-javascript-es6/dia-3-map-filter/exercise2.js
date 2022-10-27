1f227231233845f1e6f07e814f2506323f61cb78
helmatrindade <helmacaires28@gmail.com>
2022-10-27 15:04:30 -0300

    function oldBooksOrdered com filter e sort

--- fundamentos/secao-8-higher-order-functions-do-javascript-es6/dia-3-map-filter/exercise2.js
+++ fundamentos/secao-8-higher-order-functions-do-javascript-es6/dia-3-map-filter/exercise2.js
@@ -9,3 +9,16 @@ const fantasyOrScienceFiction = () => {
 );
 }
 //console.log(fantasyOrScienceFiction());
+
+
+
+// Crie um array formado pelos livros com mais de 60 anos desde sua publicação. Esse array deve ser ordenado do livro mais velho ao mais novo. use sa funções filter e sort
+
+
+const oldBooksOrdered = () => {
+  const year = new Date().getFullYear(); // const com o ano atual
+  return books.filter((book) => {    //HOF
+    return year - book.releaseYear > 60; // subtração do ano atual menos o ano de lançamento do livro, e que seja maior que 60 anos
+  }).sort((a, b) => a.releaseYear - b.releaseYear); //Ordenando do menor para o maior.
+};
+//console.log(oldBooksOrdered());
